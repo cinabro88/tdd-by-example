8a0124803ed4d34c6715dd7c77fec6944232ccef
김민국 <mgkim@woowahan.com>
2015-11-30 16:33:37 +0900

    add .gitignore

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -6,14 +6,26 @@ To-Do
 - $5 + 10CHF = $10 (환율이 2:1일 경우)
 - [v] $5 * 2 = $10
 - amount를 private
-- Dollar 부작용?
+- [v] Dollar 부작용?
 - Money 반올림?
+- [v] equals()
+- hashCode()
+- Equal null
+- Equal object
 */
 public class DollarTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
