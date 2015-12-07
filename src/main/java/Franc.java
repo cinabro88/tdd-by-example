d96be851da6ba49550ec6ea480a22cdab50d976c
김민국 <mgkim@woowahan.com>
2015-12-07 23:11:24 +0900

    ~11장 완료.

--- src/main/java/Franc.java
+++
@@ -1,9 +0,0 @@
-public class Franc extends Money {
-    public Franc(int amount) {
-        this.amount = amount;
-    }
-
-    public Franc times(int multiplier) {
-        return new Franc(this.amount * multiplier);
-    }
-}
--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,9 +1,35 @@
 public class Money {
     protected int amount;
+    protected String currency;
+    
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+    
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+    
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
     
     public boolean equals(Object object) {
         Money money  = (Money) object;
-        return this.amount == money.amount && 
-            getClass().equals(money.getClass());
+        return this.amount == money.amount 
+            && this.currency().equals(money.currency());
+    }
+    
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+    
+    public String currency() {
+        return currency;
+    }
+    
+    public String toString() {
+        return amount + " " + currency;
     }
 }
